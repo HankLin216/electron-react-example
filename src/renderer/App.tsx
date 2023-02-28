4aa701679f44d3c5c81d0ed9313cd3a92a6c36f2
HankLin(laptop) <hanklin21000@gmail.com>
2023-02-28 16:08:29 +0800

    Add: routes

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,50 +1,43 @@
-import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import RootLayout from './layouts/RootLayout';
+import Overview from './pages/Overview';
 import './App.css';
 
-function Hello() {
-  return (
-    <div>
-      <div className="Hello">
-        <img width="200" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="folded hands">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
-      </div>
-    </div>
-  );
+function Setting() {
+  return <div>Setting</div>;
 }
+
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <Overview /> },
+      { path: '/Settings', element: <Setting /> },
+    ],
+  },
+]);
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'light',
+  },
+  components: {
+    MuiTabs: {
+      styleOverrides: {
+        indicator: {
+          // left: 0,
+        },
+      },
+    },
+  },
+});
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Hello />} />
-      </Routes>
-    </Router>
+    <ThemeProvider theme={darkTheme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   );
 }
